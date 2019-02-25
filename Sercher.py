cee24959ed2935980f4c8911667ba522a751cb26
Umisyo <kusunokisouta@gmail.com>
2019-02-25 17:31:07 +0900

    'staticmethodにした'

--- Sercher.py
+++ Sercher.py
@@ -4,15 +4,16 @@ import random
 blue_df = pd.read_csv('blue_sky_books.csv', encoding='utf-8')
 
 
-class word_serch:
-    def Sercher(self, wont_serch: str, serch_word: str):
+class word_search:
+    @staticmethod
+    def Searcher(wont_serch: str, serch_word: str):
         title_df = blue_df[blue_df[wont_serch].str.contains(serch_word)]
 
         return title_df.to_json(force_ascii=False)
 
-class random_serch:
-    def Sercher(self):
-        ls = range(1, 16224)
+    @staticmethod
+    def Random_searcher():
+        ls = list(range(1, 16224))
         random_df = blue_df.take([random.choice(ls)])
 
         return random_df
